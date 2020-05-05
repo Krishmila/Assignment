681263ba9eae4d518b0c3ce261098938a2433207
Krishmila <madbkjayasinghe97@gmail.com>
2020-05-05 08:41:52 +0530

    changes in servlet

--- Doctor_REST/src/com/caremarque/doctor/servlet/DoctorAPI.java
+++ Doctor_REST/src/com/caremarque/doctor/servlet/DoctorAPI.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.caremarque.doctor.model.Doctor;
 import com.caremarque.doctor.service.DoctorServiceImpl;
+import com.caremarque.doctor.util.CommonUtils;
 
 /**
  * Servlet implementation class DoctorAPI
@@ -36,80 +37,8 @@ public class DoctorAPI extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-		Doctor doctor = new Doctor();
-		
-		doctor.setDoctorId(request.getParameter("doctorId"));
-		doctor.setFirstName(request.getParameter("firstName"));
-		doctor.setLastName(request.getParameter("lastName"));
-		doctor.setRegNo(request.getParameter("regNo"));
-		doctor.setGender(request.getParameter("gender"));
-		doctor.setSpecialization(request.getParameter("specialization"));
-		doctor.setPhone(request.getParameter("phone"));
-		doctor.setEmail(request.getParameter("email"));
-		doctor.setDoctorCharges(Double.parseDouble(request.getParameter("doctorCharges")));
-		doctor.setPassword(request.getParameter("password"));
-		doctor.setConfirmPassword(request.getParameter("confirmPassword"));
-
-		String output = doctorServiceImpl.createDoctor(doctor);
-		response.getWriter().write(output);
-		
-	}
-
-	/**
-	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
-	 */
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-		Map paras = getParasMap(request);
-		
-
-		/*System.out.println("Doctor API put method");
-		System.out.println("doctor id: " + paras.get("hideDoctorIdSave").toString());
-		System.out.println("paymeint name: " + paras.get("firstName").toString()); */
-		
-		String output = doctorServiceImpl.updateDoctor(
-		paras.get("hideDoctorIdSave").toString(),		
-		//paras.get("doctorId").toString(),
-		paras.get("firstName").toString(),
-		paras.get("lastName").toString(),
-		paras.get("regNo").toString(),
-		paras.get("gender").toString(),
-		paras.get("specialization").toString(),
-		paras.get("phone").toString(),
-		paras.get("email").toString(),
-		(double)paras.get("doctorCharges"),
-		paras.get("password").toString(),
-		paras.get("confirmPassword").toString());
-		
-		//String output = doctorServiceImpl.updateDoctor(paras);
-		response.getWriter().write(output);
-		
-	}
-
-	/**
-	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
-	 */
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-		Map paras = getParasMap(request);
-		
-		String output = doctorServiceImpl.cancelDoctor(paras.get("doctorId").toString());
-		
-		response.getWriter().write(output);
-	}
-	
 	private static Map getParasMap(HttpServletRequest request) {
 		
 		Map<String, String> map = new HashMap<String, String>();
@@ -135,5 +64,92 @@ public class DoctorAPI extends HttpServlet {
 		}
 		return map;
 	}
+    
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		Doctor doctor = new Doctor();
+		DoctorServiceImpl ob=new DoctorServiceImpl();
+		String doctorId = CommonUtils.generateDoctorIDs(ob.getDoctorIDs());
+		System.out.println("check");
+		
+		doctor.setDoctorId(doctorId);
+		doctor.setFirstName(request.getParameter("firstName"));
+		System.out.println("Check Name"+request.getParameter("firstName"));
+		doctor.setLastName(request.getParameter("lastName"));
+		doctor.setRegNo(request.getParameter("regNo"));
+		doctor.setGender(request.getParameter("gender"));
+		doctor.setSpecialization(request.getParameter("specialization"));
+		doctor.setPhone(request.getParameter("phone"));
+		doctor.setEmail(request.getParameter("email"));
+		System.out.println("Check charges"+request.getParameter("doctorCharges"));
+		try {
+		doctor.setDoctorCharges(Integer.parseInt(request.getParameter("doctorCharges")));
+		}catch(NumberFormatException e) {System.out.println(e);}
+		doctor.setPassword(request.getParameter("password"));
+		doctor.setConfirmPassword(request.getParameter("confirmPassword"));
+
+		String output = doctorServiceImpl.createDoctor(doctor);
+		System.out.println(output);
+		response.getWriter().write(output);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		Map paras = getParasMap(request);
+		
+
+		/*System.out.println("Doctor API put method");
+		System.out.println("doctor id: " + paras.get("hideDoctorIdSave").toString());
+		System.out.println("paymeint name: " + paras.get("firstName").toString()); */
+		DoctorServiceImpl ob=new DoctorServiceImpl();
+		String doctorId = CommonUtils.generateDoctorIDs(ob.getDoctorIDs());
+		System.out.println("Doctor Chargers: " +  paras.get("doctorCharges"));
+		
+		String output = doctorServiceImpl.updateDoctor(
+		//paras.get("hidDoctorIdSave").toString(),		
+		paras.get("doctorId").toString(),
+		paras.get("firstName").toString(),
+		paras.get("lastName").toString(),
+		paras.get("regNo").toString(),
+		paras.get("gender").toString(),
+		paras.get("specialization").toString(),
+		paras.get("phone").toString(),
+		paras.get("email").toString(),
+		(int)paras.get("doctorCharges"),
+		paras.get("password").toString(),
+		paras.get("confirmPassword").toString());
+		
+		//String output = doctorServiceImpl.updateDoctor(paras);
+		response.getWriter().write(output);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		Map paras = getParasMap(request);
+		
+		String output = doctorServiceImpl.cancelDoctor(paras.get("doctorID").toString());
+		
+		response.getWriter().write(output);
+	}
+	
 
 }
